fdf6a2396c76b150d1d1f0719e8986c2b7a75a11
albertotn <2526457+albertotn@users.noreply.github.com>
2019-10-02 22:06:38 +0200

    added execution time in csv file

--- src/main/java/AllocationCsvExporter.java
+++ src/main/java/AllocationCsvExporter.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.LongSummaryStatistics;
+import java.util.stream.Collectors;
 
 public class AllocationCsvExporter {
 
@@ -15,7 +16,7 @@ public class AllocationCsvExporter {
 
     private AllocationCsvExporter() {}
 
-    public void writeAllocationsToCsv(Maven3Version maven3Version, long[] allocations, String resultFilePath) throws IOException {
+    public void writeAllocationsToCsv(Maven3Version maven3Version, AllocationTimePair[] allocations, String resultFilePath) throws IOException {
 
         int numberOfAllocations = allocations.length;
         CSVFormat csvFormat = buildCsvFormat(resultFilePath, numberOfAllocations);
@@ -37,7 +38,7 @@ public class AllocationCsvExporter {
     }
 
     private String[] buildCsvHeaders(int numberOfAllocations) {
-        String[] csvHeaders = new String[(2 * numberOfAllocations) + 5];
+        String[] csvHeaders = new String[(3 * numberOfAllocations) +5];
         csvHeaders[0] = "Maven version";
         csvHeaders[1] = "Average (bytes)";
         csvHeaders[2] = "Average (Gb)";
@@ -47,32 +48,40 @@ public class AllocationCsvExporter {
         for (int i = 1; i < numberOfAllocations + 1; i++) {
             csvHeaders[i + 4] = "Allocation" + " " + i + " "+ "(bytes)";
         }
+        
+        for (int i = 1; i < numberOfAllocations + 1; i++) {
+        	csvHeaders[i + 4+ numberOfAllocations] = "Lenght" + " " + i + " "+ "(seconds)";
+        }
 
         for (int i = 1; i < numberOfAllocations + 1; i++) {
-            csvHeaders[i + 4 + numberOfAllocations] = "Allocation" + " " + i + " "+ "(Gb)";
+            csvHeaders[i + 4 + numberOfAllocations*2] = "Allocation" + " " + i + " "+ "(Gb)";
         }
         return csvHeaders;
     }
 
-    private List<Object> buildCsvRecord(Maven3Version maven3Version, long[] allocations) {
+    private List<Object> buildCsvRecord(Maven3Version maven3Version, AllocationTimePair[] input) {
 
-        List<Object> csvRecord = new ArrayList<>((2 * allocations.length) + 5);
+        List<Object> csvRecord = new ArrayList<>((2 * input.length) + 5);
 
         csvRecord.add(maven3Version);
 
-        LongSummaryStatistics allocationStatistics = Arrays.stream(allocations).summaryStatistics();
+        LongSummaryStatistics allocationStatistics = Arrays.stream(input).collect(Collectors.summarizingLong(AllocationTimePair::getAllocationInBytes));
         double averageAllocationInBytes = allocationStatistics.getAverage();
         csvRecord.add(averageAllocationInBytes);
         csvRecord.add(averageAllocationInBytes / Math.pow(1024, 3));
         csvRecord.add(allocationStatistics.getMin());
         csvRecord.add(allocationStatistics.getMax());
 
-        for (int i = 0; i < allocations.length; i++) {
-            csvRecord.add(allocations[i]);
+        for (int i = 0; i < input.length; i++) {
+            csvRecord.add(input[i].getAllocationInBytes());
         }
-
-        for (int i = 0; i < allocations.length; i++) {
-            csvRecord.add(allocations[i] / Math.pow(1024, 3));
+        
+        for (int i = 0; i < input.length; i++) {
+        	csvRecord.add(input[i].getLenghtInSeconds());
+        }         
+        
+        for (int i = 0; i < input.length; i++) {
+            csvRecord.add(input[i].getAllocationInBytes() / Math.pow(1024, 3));
         }
 
         return csvRecord;
---
+++ src/main/java/AllocationTimePair.java
@@ -0,0 +1,21 @@
+
+public class AllocationTimePair {
+
+	private final Long allocationInBytes;
+	private final  Long lenghtInSeconds;
+	
+	public AllocationTimePair(Long allocationInBytes, Long lenghtInSeconds) {
+		this.allocationInBytes = allocationInBytes;
+		this.lenghtInSeconds = lenghtInSeconds;
+	}
+
+	public Long getAllocationInBytes() {
+		return allocationInBytes;
+	}
+
+	public Long getLenghtInSeconds() {
+		return lenghtInSeconds;
+	}
+	
+	
+}
