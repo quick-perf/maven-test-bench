a26b937517ee9531fba9485bfb8bf5f583ad5aff
Patrice Cavezzan <pcavezzan@gmail.com>
2019-10-04 20:05:56 +0200

    Install project under test.

--- src/main/java/ProjectUnderTest.java
+++ src/main/java/ProjectUnderTest.java
@@ -1,11 +1,10 @@
+import net.lingala.zip4j.ZipFile;
+import net.lingala.zip4j.model.FileHeader;
+import org.apache.commons.io.FileUtils;
+
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.URL;
-import java.nio.channels.Channels;
-import java.nio.channels.FileChannel;
-import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Files;
 
 public class ProjectUnderTest {
 
@@ -16,7 +15,8 @@ public class ProjectUnderTest {
         final BenchProperties bp = BenchProperties.INSTANCE;
         final ProjectUnderTest prj = new ProjectUnderTest("/tmp/camel",
                 "https://github.com/apache/camel/archive/camel-2.23.4.zip");
-        prj.downloadProject();
+        final String path = prj.installProject();
+        System.out.println(path);
     }
 
     public ProjectUnderTest(String projectPath, String downloadFrom) {
@@ -24,10 +24,23 @@ public class ProjectUnderTest {
         this.downloadZipSource = downloadFrom;
     }
 
-    private String downloadProject() throws IOException {
-        // IOUtils.download()
-        return null;
+    public String installProject() throws IOException {
+        String projectPath = this.path;
+
+        final String zipLocalPath = IOUtils.download(this.downloadZipSource, FileUtils.getTempDirectoryPath());
+        final ZipFile zipFile = new ZipFile(zipLocalPath);
+        zipFile.extractAll(this.path);
+
+        final FileHeader rootFileHeader = zipFile.getFileHeaders().get(0);
+        if (rootFileHeader.isDirectory()) {
+            System.out.println(rootFileHeader.getFileName());
+            projectPath = this.path + File.separator + rootFileHeader.getFileName();
+        }
+
+        return projectPath;
     }
 
-
+    public String getPath() {
+        return path;
+    }
 }
